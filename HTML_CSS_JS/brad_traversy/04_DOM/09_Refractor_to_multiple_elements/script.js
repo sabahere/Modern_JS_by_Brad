ead8c5760503f760f593410353933ff15a738da9
Saba Hussain <sabahusain2001@gmail.com>
2023-10-17 15:02:27 +0530

    Refactor to multiple element

---
+++ HTML_CSS_JS/brad_traversy/04_DOM/09_Refractor_to_multiple_elements/script.js
@@ -0,0 +1,24 @@
+//Refactor to Multiple functions
+//Creating multiple function
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+function createNewItem(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+  li.className = "item";
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  document.querySelector(".items").appendChild(li);
+}
+createNewItem("Mango");
