619de303c264f45333a8bf42cf7d40881b19b23d
Saba Hussain <sabahusain2001@gmail.com>
2023-12-26 21:32:26 +0530

    AJAX and XHR

---
+++ 07_Asynchronous_JS/04-ajax-xhr/script.js
@@ -0,0 +1,26 @@
+//AJAX and XHR
+const xhr = new XMLHttpRequest();
+
+//xhr.open("GET", "./movies.json");
+xhr.open("GET", "https://api.github.com/users/sabahere/repos");
+
+//readystate have 5 possible values
+// 0 : request not initiated
+// 1 : server connection established
+// 2 : request recieved
+// 3 : processing request
+// 4 : request finished and response is ready
+
+xhr.onreadystatechange = function () {
+  if ((this.readyState === 4) & (this.status === 200)) {
+    console.log(JSON.parse(this.responseText));
+    const data = JSON.parse(this.responseText);
+    data.forEach((repo) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.owner.repos_url}`;
+      document.querySelector("#results").appendChild(li);
+    });
+  }
+};
+
+xhr.send();
