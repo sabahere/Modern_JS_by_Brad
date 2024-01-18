c10c464705b399363096e115245b0bdfe36ceb81
Saba Hussain <sabahusain2001@gmail.com>
2024-01-19 02:45:51 +0530

    getters and setters with classes

---
+++ 11_OOP_classes/05_getters_and_setters_with_classes/script.js
@@ -0,0 +1,36 @@
+class Person {
+  constructor(firstName, lastName) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  get firstName() {
+    return this.capitalizeFirst(this._firstName);
+  }
+
+  set firstName(value) {
+    this._firstName = this.capitalizeFirst(value);
+  }
+  get lastName() {
+    return this.capitalizeFirst(this._lastName);
+  }
+
+  set lastName(value) {
+    this._lastName = this.capitalizeFirst(value);
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  capitalizeFirst(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
+}
+
+const person1 = new Person("saba", "hussain");
+person1.firstName = "babi";
+person1.lastName = "zooti";
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.fullName);
